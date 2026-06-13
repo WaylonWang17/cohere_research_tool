517416513d234d981828e0890fbcd12dc3d54516
waylon wang <waylon.wang17@gmail.com>
2026-06-13 19:35:15 -0400

    added wikipedia search for bg knowledge

--- research_tool.py
+++ research_tool.py
@@ -1,6 +1,8 @@
 import os
 import cohere
 import json
+import wikipediaapi
+import requests
 
 from exa_py import Exa
 from dotenv import load_dotenv
@@ -14,10 +16,27 @@ if not question:
 
 load_dotenv()
 
+# clients
 exa = Exa(os.getenv("EXA_API_KEY"))
 co = cohere.ClientV2(os.getenv("COHERE_API_KEY"))
+wiki = wikipediaapi.Wikipedia(user_agent='research_tool (waylon.wang17@gmail.com)', language='en')
+
+def wikipedia_search(query):
+    '''
+    background knowledge
+    '''
+    results = wiki.search(query, limit=1)
+    if not results.pages:
+        return [{'error': f'No wikipedia page found for {query}'}]
+
+    page = next(iter(results.pages.values()))
+    return [{"url": page.fullurl, "title": page.title, "summary": page.summary}]
+
 
 def exa_func(prompt):
+    '''
+    up to date info
+    '''
     response = exa.search(
         prompt,
         type="auto",
@@ -28,14 +47,15 @@ def exa_func(prompt):
         for r in response.results
     ]
 
-functions_map = {"exa": exa_func} #maps string to function name so function can be called
+functions_map = {"exa": exa_func,
+                 "wikipedia": wikipedia_search} #maps string to function name so function can be called
 
 tools = [
     {
         "type": "function",
         "function": {
             "name": "exa", #has to match key in functions_map
-            "description": "does research",
+            "description": "searches the live web for current/recent information",
             #parameters is a schema that must have type object
             "parameters": {
                 "type": "object",
@@ -48,6 +68,23 @@ tools = [
                     }
                 },
                 "required": ["prompt"],
+            },
+        },
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "wikipedia",
+            "description": "looks up background information on a topic from wikipedia",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "query": {
+                        "type": "string",
+                        "description": "the topic to look up on wikipedia",
+                    }
+                },
+                "required": ["query"],
             },
         },
     },
@@ -68,6 +105,7 @@ if response.message.tool_calls:
     messages.append(response.message) #append message asking for tool in messages
     for tc in response.message.tool_calls: #loops through all tool calls
         function_name = tc.function.name #inside of tools => function => name
+        print(f"[tool call] {function_name}({tc.function.arguments})")  # debug to see which tool got called
         function_arguments = json.loads(tc.function.arguments) #parse argument from json string into python dict. Model generates '{"prompt": "linkedin influencer with most followers"}' and json.laod changes to {"prompt": "linkedin influencer with most followers"}
         function_to_call = functions_map[function_name] #look up python function by name 
         tool_result = function_to_call(**function_arguments) #unpack argument dict as keyword args converts {"prompt": "linkedin influencer with most followers"} to prompt=linkedin influencer with most followers
